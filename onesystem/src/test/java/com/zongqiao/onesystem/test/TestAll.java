37402113ce63c510e9865c786f454dfede5411a9
Max <813776775@qq.com>
2018-04-05 16:52:48 +0800

    update test

--- onesystem/src/test/java/com/zongqiao/onesystem/test/TestAll.java
+++ onesystem/src/test/java/com/zongqiao/onesystem/test/TestAll.java
@@ -1,13 +1,27 @@
 package com.zongqiao.onesystem.test;
 
-import org.junit.Test;
-
 import com.zongqiao.onesystem.util.VerificationCodeUtil;
 
 public class TestAll {
   
 public static void main(String[] args) {
-	VerificationCodeUtil util = 	VerificationCodeUtil.getInstance();
-	System.err.println(util + ":::" + util.getVerificationCode());
+	
+	
+	Thread code1 = new Code();
+	Thread code2 = new Code();
+	Thread code3 = new Code();
+	code1.start();
+	code2.start();
+	code3.start();
+	
+	
 }
 }
+
+class  Code extends Thread{
+	@Override
+	public void run() {
+		VerificationCodeUtil util = VerificationCodeUtil.getInstance();
+		System.err.println(util + ":::" + util.getVerificationCode());
+	}
+} 
