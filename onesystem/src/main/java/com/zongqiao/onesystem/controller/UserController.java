21262ddc0d202d179ee441e70cd6aa11f56894b2
Max <813776775@qq.com>
2018-04-10 21:58:16 +0800

    update from home

--- onesystem/src/main/java/com/zongqiao/onesystem/controller/UserController.java
+++ onesystem/src/main/java/com/zongqiao/onesystem/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.zongqiao.onesystem.controller;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.zongqiao.onesystem.domain.User;
@@ -14,37 +18,61 @@ import com.zongqiao.onesystem.util.VerificationCodeUtil;
 /**
  * 
  * 用户控制层
+ * 
  * @author jkx
  *
  */
 @Controller
-@RequestMapping(value="/user")
-public class UserController {
-	//注册
-    @RequestMapping(value="/register.do")
-	public void register(User user,String code){
-    	System.out.println("user:" + user + "<-->code" + code);
+@RequestMapping(value = "/user")
+public class UserController
+{
+	// 注册
+	@RequestMapping(value = "/register.do")
+	public void register(User user, String code)
+	{
+		System.out.println("user:" + user + "<-->code" + code);
 	}
-    //获取验证码
-    @ResponseBody
-    @RequestMapping(value="/getCheckCode.do")
-    public String getCheckCode(HttpServletRequest request,String phoneNumber){
-    	System.out.println("phoneNumber->" + phoneNumber);
-    	HttpSession session = request.getSession();
-    	//初始化返回值
-    	String code = "";
-        //获取session中：sessionId、手机号、验证码
-    	String sessionId = session.getId();
-    	System.out.println("sessionId->" + sessionId);
-    	String sessionCode = sessionId + "-" + phoneNumber;
-    	System.out.println("sessionCode->" + sessionCode);
-    	System.out.println("session-code->" + session.getAttribute(sessionCode));
-        code = VerificationCodeUtil.getInstance().getVerificationCode();
-        session.setAttribute(sessionCode, sessionId + "-" + phoneNumber + "-" + code);
-        System.out.println(code);
-        System.out.println("放在session中的验证码->" + session.getAttribute(sessionCode));
-    	return code;
-    }
-	
-	
+
+	// 获取验证码
+	@ResponseBody
+	@RequestMapping(value = "/getCheckCode.do")
+	public String getCheckCode(HttpSession session, String phoneNumber)
+	{
+		//验证码
+		String code = null;
+		//是否存活（true:无需生成，false需要重新生成）
+		boolean isActive = false;
+		//session中验证码属性名称和值
+		String sessionCheckCodeName = session.getId() + "-" + phoneNumber;
+		String sessionCheckCodeValue = null;
+		if (session.getAttribute(sessionCheckCodeName) != null)
+		{
+			sessionCheckCodeValue = session.getAttribute(sessionCheckCodeName).toString();
+		}
+		if (sessionCheckCodeValue != null)
+		{
+			String[] strArr = sessionCheckCodeValue.split("-");
+			String sessionCheckCode = strArr[0];
+			long createTime = Long.parseLong(strArr[1]);
+			long nowTime = new Date().getTime();
+			long timeDifference = (nowTime - createTime) / 1000;
+			if (timeDifference < 60)
+			{
+				// session中存在且未过期的验证码可以直接返回
+				code = sessionCheckCode;
+				isActive = true;
+			}
+		}
+
+		// 其他情况（session中不存在、session中存在但已经过期、用户换手机号获取验证码）均需要重新生成
+		if (!isActive)
+		{
+			code = RandomStringUtils.randomNumeric(6);
+			long createTime = new Date().getTime();
+			session.setAttribute(sessionCheckCodeName, code + "-" + createTime);
+		}
+
+		return code;
+	}
+
 }
