de64b3b04a3bc6663ccdcd6c035c8da8b3c29ee3
Max <813776775@qq.com>
2018-04-11 12:16:47 +0800

    update  UserController

--- onesystem/src/main/java/com/zongqiao/onesystem/controller/UserController.java
+++ onesystem/src/main/java/com/zongqiao/onesystem/controller/UserController.java
@@ -2,18 +2,14 @@ package com.zongqiao.onesystem.controller;
 
 import java.util.Date;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang.RandomStringUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.zongqiao.onesystem.domain.User;
-import com.zongqiao.onesystem.util.VerificationCodeUtil;
 
 /**
  * 
