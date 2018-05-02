965aff298345cbcf758af60bda8e943a81f7d3d8
Max <813776775@qq.com>
2018-05-02 21:12:54 +0800

    up

--- onesystem/src/test/java/com/zongqiao/onesystem/test/LogTest.java
+++ onesystem/src/test/java/com/zongqiao/onesystem/test/LogTest.java
@@ -11,7 +11,7 @@ public class LogTest {
 	    // 记录info级别的信息    
 	    logger.info("This is info message.");    
 	    // 记录error级别的信息    
-	    logger.error("This is error message."); 
+	    logger.error("This is error message.");
 	    System.out.println("===========");
 	}
 }
