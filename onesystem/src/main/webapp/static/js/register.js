e96a800e6a6b379fcd6bc4fe7ac47c285d4a948b
Max <813776775@qq.com>
2018-05-03 23:22:15 +0800

    up

--- onesystem/src/main/webapp/static/js/register.js
+++ onesystem/src/main/webapp/static/js/register.js
@@ -1,16 +1,13 @@
-$(function(){
-	//<a href="#" onClick="getCheckCode();">获取验证码</a>
+new Vue({
+  el : '#registerForm',
+  data : {
+	  telephone : '',
+	  checkCode : "123456"
+  },
+  methods:{
+	  submitForm:function(){
+		  alert(this.telephone + ":::::"  + this.checkCode);
+		  return;
+	  }
+  }
 });
-function getCheckCode(){
-	$.ajax({
-		url:'user/getCheckCode.do',
-		data:{'phoneNumber':'15324490545'},
-		type:'post',
-		success:function(data){
-			alert('success->'+ data);
-		},
-		error:function(){
-			console.log('ajax调用失败');
-		}
-	});
-}
