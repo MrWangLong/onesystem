e4ec20bf27eaa3d0b12ea121215f74919b7ef232
Max <813776775@qq.com>
2018-06-06 22:25:38 +0800

    up

--- onesystem-web/src/components/user/Login.vue
+++ onesystem-web/src/components/user/Login.vue
@@ -9,12 +9,12 @@ export default {
   name: 'Login',
   data () {
     return {
-
+     isShow:false
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add "scoped" attribute to limit CSS to this component only. -->
 <style scoped>
 </style>
